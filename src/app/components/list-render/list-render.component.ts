204781081aeaf95516e4c3cbba9730b138066f10
Alan-LR <alanlopes.r@gmail.com>
2023-01-11 16:53:39 -0300

    Revert "Aula 13 Services do Angular"
    This reverts commit a7284893e7fd6f05f6fcac01ca4ec71f8096540d.

--- src/app/components/list-render/list-render.component.ts
+++ src/app/components/list-render/list-render.component.ts
@@ -1,4 +1,3 @@
-import { ListService } from './../../services/list.service';
 import { Component } from '@angular/core';
 import { Animal } from '../interfaces/Animal';
 
@@ -9,22 +8,16 @@ import { Animal } from '../interfaces/Animal';
 })
 export class ListRenderComponent {
 
-  animals: Animal[] = [
-    { name: 'bob', type: 'cachorro', age: 4 },
-    { name: 'belinha', type: 'gato', age: 2 },
-    { name: 'quindim', type: 'ratão', age: 1 }
+  animals:  Animal[] = [
+    {name: 'bob', type: 'cachorro', age:4},
+    {name: 'belinha', type: 'gato', age:2},
+    {name: 'quindim', type: 'ratão', age:1}
   ]
 
   animalDetails: string = ''
 
-  constructor(private listService: ListService) { }
-
-  showAge(animal: Animal) {
+  showAge(animal: Animal){
     this.animalDetails = 'O pet ' + animal.name + ' Tem ' + animal.age + ' anos';
-  }
-
-  removeAnimal(animal: Animal){
-    this.animals =  this.listService.remove(this.animals, animal)
   }
 
 }
