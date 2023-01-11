e459401a6955e73f1966088c74c6c89f143adc29
Alan-LR <alanlopes.r@gmail.com>
2023-01-11 16:33:27 -0300

    Aula 15 Two way data binding no Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,20 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { FirstComponentComponent } from './components/first-component/first-component.component';
-import { ParentDataComponent } from './components/parent-data/parent-data.component';
-import { DirectivesComponent } from './components/directives/directives.component';
-import { IfRenderComponent } from './components/if-render/if-render.component';
-import { EventosComponent } from './components/eventos/eventos.component';
-import { EmitterComponent } from './components/emitter/emitter.component';
 import { ChangeNumberComponent } from './components/change-number/change-number.component';
+import { DirectivesComponent } from './components/directives/directives.component';
+import { EmitterComponent } from './components/emitter/emitter.component';
+import { EventosComponent } from './components/eventos/eventos.component';
+import { FirstComponentComponent } from './components/first-component/first-component.component';
+import { IfRenderComponent } from './components/if-render/if-render.component';
 import { ListRenderComponent } from './components/list-render/list-render.component';
+import { ParentDataComponent } from './components/parent-data/parent-data.component';
 import { PipesComponent } from './components/pipes/pipes.component';
+import { TwoWayBindingComponent } from './components/two-way-binding/two-way-binding.component';
 
 @NgModule({
   declarations: [
@@ -25,12 +27,14 @@ import { PipesComponent } from './components/pipes/pipes.component';
     EmitterComponent,
     ChangeNumberComponent,
     ListRenderComponent,
-    PipesComponent
+    PipesComponent,
+    TwoWayBindingComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
